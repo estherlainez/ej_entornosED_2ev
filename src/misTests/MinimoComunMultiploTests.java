71b27ea6625abcacd72166633f2818f5f40fd279
Esther <esthertarazona81@gmail.com>
2019-03-15 20:59:04 +0100

    en esta carpeta tengo los dos paquetes de codigo y tests

---
+++ src/misTests/MinimoComunMultiploTests.java
@@ -0,0 +1,38 @@
+package misTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import code.MinimoComunMultiplo;
+
+class MinimoComunMultiploTests {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testMinimoComunMultiplo() {
+		MinimoComunMultiplo mcm = new MinimoComunMultiplo();
+		int resultado=0;
+		int[]candidatos= {6,8};
+		assertEquals(resultado,0);
+	}
+}
---
+++ src/misTests/VerificaNumerosAmigosTest.java
@@ -0,0 +1,39 @@
+package misTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import code.VerificaNumerosAmigos;
+
+class VerificaNumerosAmigosTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		int a=5,b=10;
+		boolean respuesta=false;
+		VerificaNumerosAmigos amigos = new VerificaNumerosAmigos();
+		assertEquals(respuesta,false);
+	}
+
+}
